1277d8bd106a4e00c5970ac16ee65e9eabeec663
nilim <hello@MacBooks-MacBook-Pro.local>
2019-03-16 17:03:53 +0600

    space issue

--- src/core/db/db.ts
+++ src/core/db/db.ts
@@ -2,7 +2,7 @@ import {ConnectionOptions, createConnection} from 'typeorm';
 
 export async function dbInitializer(options:ConnectionOptions):Promise<any>{
     const connecction = await createConnection(options)
-    .then(async connection=>{
+    .then(async connection => {
         console.log('MongoDB Database connected');
     })
     .catch(error => console.log('error', error));
