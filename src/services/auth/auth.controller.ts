171a76edd25176cb982b8b4b29d528314f2f2ff0
snanilim <snanilim@gmail.com>
2019-04-18 16:54:48 +0600

    try catch error solve

--- src/services/auth/auth.controller.ts
+++ src/services/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import * as service from './auth.service';
 
 export const register = async (req, res, next) => {
     try {
+        // const aaaa;
         const response = await service.register(req.body);
         res.send(response);
     } catch (error) {
--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -4,16 +4,20 @@ import User from '../user/user.model';
 import RefreshToken from './refreshToken';
 
 const generateToken = (user, accessToken) => {
-    const tokenType = 'Bearer';
-    const refreshToken = RefreshToken.generate(user).token; // its only reply token from full obj
-    const expireTime = moment().add(config.get('jwtTimeExpire'), 'minutes').unix();
+    try {
+        const tokenType = 'Bearer';
+        const refreshToken = RefreshToken.generate(user).token; // its only reply token from full obj
+        const expireTime = moment().add(config.get('jwtTimeExpire'), 'minutes').unix();
 
-    return {
-        tokenType,
-        refreshToken,
-        accessToken,
-        expireTime,
-    };
+        return {
+            tokenType,
+            refreshToken,
+            accessToken,
+            expireTime,
+        };
+    } catch (error) {
+        throw error;
+    }
 };
 
 export const register = async (data) => {
@@ -22,11 +26,10 @@ export const register = async (data) => {
             email: data.email,
             password: data.password
         });
-        const resSaveUser = user.save();
+        const resSaveUser = await user.save();
         const token = generateToken(resSaveUser, resSaveUser.token);
         return token;
     } catch (error) {
-        console.log('errror', error);
         throw User.checkDuplicateEmail(error);
     }
 
